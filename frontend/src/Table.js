999b344edcfc89d59ec61529cb7ca9babc52aa80
KijkEr <joep_is_cool@hotmail.com>
2023-08-13 21:57:58 +0200

    bijwerken

--- frontend/src/Table.js
+++ frontend/src/Table.js
@@ -1,35 +1,31 @@
 import React from "react";
 
 function Table(props) {
-  const { total_memory, free_memory, cpu_count, cpu_usage } = props.data;
+  const { data } = props;
 
-  const cpuUsageRows = cpu_usage.map((usage, index) => (
-    <tr key={index}>
-      <td>{index + 1}</td>
-      <td>{usage.toFixed(2)}%</td>
-    </tr>
-  ));
+  const headers = Object.keys(data);
+  const rows = headers.map((header, index) => {
+    if (header === "cpu_usage") {
+      const cpuUsageRows = data[header].map((usage, index) => (
+        <tr key={index}>
+          <td>{`CPU ${index + 1}`}</td>
+          <td>{`${usage.toFixed(2)}%`}</td>
+        </tr>
+      ));
+      return cpuUsageRows;
+    } else {
+      return (
+        <tr key={index}>
+          <td>{header}</td>
+          <td>{data[header]}</td>
+        </tr>
+      );
+    }
+  });
 
   return (
     <table>
-      <tbody>
-        <tr>
-          <td>Total Memory:</td>
-          <td>{total_memory}</td>
-        </tr>
-        <tr>
-          <td>Free Memory:</td>
-          <td>{free_memory}</td>
-        </tr>
-        <tr>
-          <td>CPU Count:</td>
-          <td>{cpu_count}</td>
-        </tr>
-        <tr>
-          <td colSpan={2}>CPU Usage:</td>
-        </tr>
-        {cpuUsageRows}
-      </tbody>
+      <tbody>{rows}</tbody>
     </table>
   );
 }
